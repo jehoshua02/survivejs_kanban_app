ed880d3c87571571296922a96f20ee03bdd39656
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-05-27 02:50:18 -0600

    Add Dummy Item

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import Note from './Note';
+import Notes from './Notes';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        { task: 'Learn Webpack' },
+        { task: 'Learn React' },
+        { task: 'Do Laundry' },
+      ]
+    };
+  }
+
   render() {
-    return <Note />;
+    var notes = this.state.notes;
+    return (
+      <div>
+        <button onClick={p.addItem.bind(this)}>+</button>
+        <Notes items={notes} />
+      </div>
+    );
   }
 }
+
+var p = {
+  addItem() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        task: 'New task'
+      }])
+    });
+  }
+};
---
+++ app/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Note from './Note';
+
+export default class Notes extends React.Component {
+  render() {
+    var notes = this.props.items;
+
+    return (
+      <ul className='notes'>{notes.map((note, i) =>
+        <li className='note' key={'note' + i}>
+          <Note task={note.task} />
+        </li>
+      )}</ul>
+    );
+  }
+}
